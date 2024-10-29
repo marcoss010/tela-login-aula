e10aa6cbcc758f022a6c2835827f362019005ba8
Marquinnnhos001 <marcos.magnani@aluno.senai.br>
2024-10-29 12:01:15 -0300

    Login Verificação

---
+++ js/script.js
@@ -0,0 +1,85 @@
+const form = document.getElementById('form');
+const email_input = document.getElementById('email-input')
+const password_input = document.getElementById('passoword-input');
+const error_message = document.getElementById('error-message');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let errors = [];
+    errors = validateLoginForm(
+        email_input.value,
+        password_input.value
+    );
+
+    //Verificar Login
+    if (errors.length ===0) {
+        const isValideLogin = verifyLogin(
+             email_input.value,
+             password_input.value
+        );
+        if (!isValideLogin) {
+            errors.push ('email ou senha incorretos')
+        }
+    }
+
+    if (errors.length > 0) {
+        error_message.innerText = errors.join(". ");
+        return;
+    }
+
+    window.location.href = 'home.html';
+});
+
+function validateLoginForm(email, passoword) {
+    let errors = [];
+
+    if (email === "" || email == null) {
+        errors.push('email é obrigatório');
+        email_input.parentElement.classList.add("incorrect");
+    }
+
+    if (passoword === "" || email == null) {
+        errors.push('senha é obrigatório')
+        password_input.parentElement.classList.add("incorrect");
+    }
+
+    return errors;
+}
+
+const allInputs = [
+    email_input,
+    password_input,
+  ].filter((input) => input != null);
+  
+  allInputs.forEach((input) => {
+    input.addEventListener("input", () => {
+      if (input.parentElement.classList.contains("incorrect")) {
+        input.parentElement.classList.remove("incorrect");
+        error_message.innerText = "";
+      }
+    });
+  });
+
+  // Verificar login
+function verifyLogin(email, password) {
+    const request = new XMLHttpRequest();
+    request.open("GET", "./data/users.json", false);
+    request.send(null);
+
+    localStorage.removeItem("userEmail");
+    localStorage.removeItem("userName");
+
+    if (request.status === 200) {
+        const users = JSON.parse(request.responseText);
+        const user = users.find(user => user.email === email);
+        if (user && user.password === password) {
+            localStorage.setItem("userEmail", email);
+            localStorage.setItem("userName", user.name);
+            return true;
+        }
+        return false;
+    } else {
+        console.error("Erro ao buscar usuários:", request.statusText);
+        return false;
+    }
+}
